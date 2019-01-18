164b1b82352a8134e17a4c62ed98defbeda13462
Toney Sui <charlie_5899@hotmail.com>
2019-01-18 02:31:48 -0800

    Bash script for deploying test nodes.

---
+++ deploy_test_nodes.sh
@@ -0,0 +1,9 @@
+
+cp ~/MyProjects/Git/toneyisnow/gotest/go_build_gotest_ ~/WebRoot/swarm1/
+cp ~/MyProjects/Git/toneyisnow/gotest/go_build_gotest_ ~/WebRoot/swarm2/
+cp ~/MyProjects/Git/toneyisnow/gotest/go_build_gotest_ ~/WebRoot/swarm3/
+
+cp ~/MyProjects/Git/toneyisnow/gotest/swarm-config-1.json ~/WebRoot/swarm1/swarm-config.json
+cp ~/MyProjects/Git/toneyisnow/gotest/swarm-config-2.json ~/WebRoot/swarm2/swarm-config.json
+cp ~/MyProjects/Git/toneyisnow/gotest/swarm-config-3.json ~/WebRoot/swarm3/swarm-config.json
+
